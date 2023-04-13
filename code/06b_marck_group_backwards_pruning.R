c2c0f16609797c91ebf28eb83d4bf76e0ff52abe
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-04-13 16:21:47 +0200

    Update 06b_marck_group_backwards_pruning.R

--- code/06b_marck_group_backwards_pruning.R
+++ code/06b_marck_group_backwards_pruning.R
@@ -23,17 +23,36 @@ broomed_full_model <-  broom::tidy(full_model) %>%
   
 anova_full_model <- anova(full_model, test = "Chisq") %>% 
   rownames_to_column("variable") %>% 
-  mutate(`Pr(>Chi)`= round(`Pr(>Chi)`, digits = 7)) %>%
   mutate(interactions = str_count(variable, ":") + 1) %>% 
   arrange(-interactions, -`Pr(>Chi)`) %>% 
-  mutate(sig = ifelse(`Pr(>Chi)` <0.05, "Sig", "Non Sig")) %>% 
-  dplyr::select(variable, sig, interactions, `Pr(>Chi)`, everything())
+  mutate(sig = ifelse(`Pr(>Chi)` <0.05, "Sig", "Non Sig"))
 
 write_tsv(x = broomed_full_model, file = "output/results/marck_full_model_broomed.tsv")
 write_tsv(x = anova_full_model, file = "output/results/marck_full_model_anova.tsv")
 
 ##backwards pruning
-variable_highest_p_value <- anova_full_model[1,1]
+#variable_highest_p_value <- anova_full_model[1,1]
+
+to_drop_df <- anova_full_model %>% 
+  slice_max(order_by = `Pr(>Chi)`) 
+
+#if there's more than one to kick out, kick out the one that is an interaction
+if(nrow(to_drop_df) != 1 & filter(to_drop_df, interactions == 2) %>% nrow()){
+  
+  to_drop_var <-   filter(to_drop_df, interactions == 2) %>% 
+    .$variable
+  }
+
+#if there aren't interactions, randomly pick one of the same valued ones
+if(nrow(to_drop_df) != 1 & (filter(to_drop_df, interactions == 2) %>% nrow() < 1)){
+  
+  to_drop_var <-   to_drop_df %>% 
+    sample_n(size = 1) %>% 
+    .$variable
+}
+
+
+
 
 sig_vars <- anova_full_model %>% 
   filter(sig == "Sig") %>% 
