8a334b9616e16d54263cfcfea184d3bb6cf00879
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-04-26 14:22:21 +0200

    Update fun_def_anova_drop.R

--- code/fun_def_anova_drop.R
+++ code/fun_def_anova_drop.R
@@ -3,8 +3,9 @@ source("01_requirements.R")
 #We are fitting the model by pruning the factors in a stepwise fashion with an ANOVA Chi-square test. The term that is the least significant in the ANOVA will be dropped, a new model rendered and another ANOVA until all the terms left are significant in the ANOVA. Significant here means passuing the p-value < 0.05 treshold. The term with the highest p value is dropped each time. If the term that has the highest p value also participates in an interaction that is significant, it is not dropped, the term with the second highest p value is dropped instead (if it does not also participate in an interaction that is significant, and so on).
 
 fun_anova_pick_drop <- function(model) {
+#model <- full_model
 
-anova_df <- anova(model, test = "Chisq") %>% 
+anova_df <-  anova(model, test = "Chisq") %>% 
     rownames_to_column("variable") %>% 
     mutate(interactions = str_count(variable, ":") + 1) %>% 
     arrange(-interactions, -`Pr(>Chi)`) %>% 
@@ -19,7 +20,7 @@ if(n_non_sig == 0){
   to_drop_var <- NULL
   }else{
  
-cat(paste0("There are variables which don't meet the p-value < 0.05 treshold in the anova, pruning necessary.\n"))
+    warning("There are variables which don't meet the p-value < 0.05 treshold in the anova, pruning necessary.\n")
 
    to_drop_df <- anova_df %>% 
      slice_max(order_by = `Pr(>Chi)`) 
@@ -48,7 +49,7 @@ sig_vec <- anova_df %>%
   paste(collapse = ",")
 
 if(to_drop_var %in% sig_vec){
-  cat("The variable with the highest p-value is involved in an interaction!!! Manual thinking needed. Sorry!")
+  warning("The variable with the highest p-value is involved in an interaction!!! Manual thinking needed. Sorry!")
   to_drop_var <- NULL
 }
 
