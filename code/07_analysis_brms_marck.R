d56db77533fe539953968f7a9ad7207fba3a3902
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-04-27 17:04:05 +0200

    Update 07_analysis_brms_marck.R

--- code/07_analysis_brms_marck.R
+++ code/07_analysis_brms_marck.R
@@ -8,17 +8,23 @@ data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_marck_group_scaled.t
 #inspiried by
 #https://bookdown.org/ajkurz/Statistical_Rethinking_recoded/counting-and-classification.html#binomial-regression
 
-
-output <-
-  brm(data = data, family = negbinomial,
-      lg_count  ~  Annual_precipitation_mean * Precipitation_seasonality_mean +
-        Annual_temperature_mean * Temperature_seasonality_mean +
-        Latitude_abs_mean  +
+output <-  brm(data = data, family = negbinomial,
+      lg_count  ~  Annual_precipitation_mean +
         EA033 +  Isolation + 
-        Shoreline * Settlement_date_grouping_finer +
-        Area_land * Settlement_date_grouping_finer  +
-        ratio_coastline_to_area* Settlement_date_grouping_finer,
-      prior = c(prior(normal(0, 100), class = Intercept),
-                prior(normal(0, 1), class = b)),
-      iter = 3000, warmup = 1000, chains = 4, cores = 4,
+        Shoreline * Settlement_date_grouping_finer,
+#      prior = c(prior(normal(0, 100), class = Intercept), prior(normal(0, 1), class = b)),
+      iter = 3000, 
+      warmup = 1000, 
+      chains = 4, 
+      cores = 4,
       seed = 10) 
+
+summary(output)
+
+predict_df <- data.frame(predicted =  predict(output),
+                         group = data$group) %>% 
+  mutate(min = predicted.Estimate - predicted.Est.Error) %>% 
+  mutate(max = predicted.Estimate + predicted.Est.Error)
+
+
+
