1310aa8f132645e5c4323145e39be3bf36495c90
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-05-19 14:33:50 +0200

    Update 07_analysis_brms_marck.R

--- code/07_analysis_brms_marck.R
+++ code/07_analysis_brms_marck.R
@@ -3,14 +3,12 @@ source("01_requirements_brms.R")
 
 data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_marck_group_scaled.tsv", show_col_types = F) 
 
-
-
 #inspiried by
 #https://bookdown.org/ajkurz/Statistical_Rethinking_recoded/counting-and-classification.html#binomial-regression
 
 output <-  brm(data = data, family = negbinomial,
       lg_count  ~  Annual_precipitation_mean +
-        EA033 +  Isolation + 
+        as.factor(EA033) +  Isolation + 
         Shoreline * Settlement_date_grouping_finer,
 #      prior = c(prior(normal(0, 100), class = Intercept), prior(normal(0, 1), class = b)),
       iter = 3000, 
