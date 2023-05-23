29ad759581c02f1ad603ad611d9f66e28c773a5a
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-05-23 16:58:11 +0200

    Update 02_get_DPLACE.R

--- code/02_get_DPLACE.R
+++ code/02_get_DPLACE.R
@@ -35,4 +35,31 @@ x <-  file.copy(from = old_fn_files,to = exdir, recursive = T, overwrite = T)
   unlink(old_fn, recursive = T)
   
 }
+
+dplace_language_IDs <- read_csv("data/dplace-cldf/languages.csv", show_col_types = F) %>% 
+  dplyr::select(ID, Glottocode)
+
+#data table with information on the language-levlled parent
+#
+glottolog_fn <- "output/processed_data/glottolog_language_table_wide_df.tsv"
+if(!file.exists(glottolog_fn)){
+  source("02_get_glottolog_language_table.R")}
+
+glottolog_cldf <- read_tsv(file = glottolog_fn, show_col_types = F) %>% 
+  dplyr::select(Glottocode, Language_level_ID)
+
+read_csv("data/dplace-cldf/values.csv", show_col_types = F, col_types = cols(.default = "c")) %>% 
+  dplyr::select(ID = Language_ID, Parameter_ID, Value) %>% 
+  left_join(dplace_language_IDs, by = "ID") %>% 
+  left_join(glottolog_cldf, by = "Glottocode") %>% 
+  filter(Parameter_ID == "MonthlyMeanNetPrimaryProduction" |
+           Parameter_ID == "NetPrimaryProductionPredictability") %>% 
+  mutate(Value = as.numeric(Value)) %>% 
+  group_by(Language_level_ID, Parameter_ID) %>% #there are more than one entry per language, so we take the mean
+  summarise(n = n(), 
+            mean_Value = mean(Value), .groups = "drop") %>% 
+  rename(Language_ID = Language_level_ID) %>% 
+  reshape2::dcast(Language_ID ~ Parameter_ID, value.var = "mean_Value") %>% 
+  write_tsv("output/processed_data/dplace_NPP.tsv", na = "")
+ 
   
