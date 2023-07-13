17f0258717c8e5170315c9fec69b2486d03bb514
HedvigS <hedvig.skirgard@gmail.com>
2023-07-13 18:53:19 +0200

    Update 00_run_order.R

--- code/00_run_order.R
+++ code/00_run_order.R
@@ -6,6 +6,8 @@ source("01_requirements_brms.R")
 
 source("01_check_used_functions.R")
 
+
+#GET
 source("02_subregions_glottocodes_unnesting.R")
 
 source("02_get_glottolog_language_table.R")
@@ -20,15 +22,18 @@ source("02_get_DPLACE.R")
 
 source("02_isolation_calculation.R")
 
+#WRANGLE
 source("03a_combine_data.R")
 
 source("03b_scale_data_and_SPLOM.R")
 
 source("03c_carrying_capacity_PCA.R")
 
+#PLOT
 source("04_data_maps.R")
 
 source("04_map_polygon_groups_marck_medium.R")
 
+#ANALYZE
 
 
