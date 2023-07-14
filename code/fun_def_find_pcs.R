3c7cb083ee718151645f4dc288d72e2aebf8a756
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-07-14 17:29:01 +0200

    Update fun_def_find_pcs.R

--- code/fun_def_find_pcs.R
+++ code/fun_def_find_pcs.R
@@ -8,7 +8,20 @@ find_pcs <- function(data){
     as.matrix() %>% 
     as.data.frame() %>% 
     rownames_to_column("ID")
+
+PCA_summary_importance <- t(summary(data_pca)$importance) %>%
+    as.data.frame() %>%
+    rownames_to_column("PC") %>%
+    dplyr::select(`Proportion of Variance`, everything())
   
+  #Tidying the component contributions into a long format.
+tidied_pca <- data_pca$rotation %>%
+    as.data.frame() %>% 
+    rownames_to_column("Parameter_ID") %>%
+    reshape2::melt(id.vars = "Parameter_ID") %>% 
+    dplyr::rename(PC = variable, Contribution = value) %>%
+    left_join(PCA_summary_importance,  by = "PC")
+
   #testing to evaluate the optimal number of components
   ev <- eigen(cor(data)) # get eigenEstimates
   ap <- nFactors::parallel(
@@ -34,6 +47,8 @@ find_pcs <- function(data){
   
   x <- list(data_pca_df= data_pca_df, 
             optimal_components = optimal_components, 
-            nScree_summary_string = nScree_summary_string, maxcol= maxcol)
+            nScree_summary_string = nScree_summary_string, maxcol= maxcol,
+            PCA_summary_importance = PCA_summary_importance,
+            tidied_pca = tidied_pca)
   x
 }
