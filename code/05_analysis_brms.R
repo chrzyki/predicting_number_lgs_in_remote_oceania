6079140657d513a4a00cbd9b39070378da0ebc24
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-07-19 13:14:21 +0200

    Update 05_analysis_brms.R

--- code/05_analysis_brms.R
+++ code/05_analysis_brms.R
@@ -5,8 +5,6 @@ source("fun_def_brms_analysis_5.R")
 
 ###MARCK
 data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_marck_group_scaled.tsv", show_col_types = F) %>%  
-  mutate("Combined_CCPC1_Size_EA033" = Carrying_capactiy_PC1*Shoreline*EA033) %>% 
-  mutate("Combined_CCPC2_Size_EA033" = Carrying_capactiy_PC2*Shoreline*EA033) %>% 
   rename(group = Marck_group)
 
 #inspired by
@@ -16,6 +14,8 @@ data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_marck_group_scaled.t
 #model specs
 formula <- "lg_count  ~ Carrying_capactiy_PC1 + 
   Carrying_capactiy_PC2 +
+  Carrying_capactiy_PC1:Shoreline:EA033 +
+  Carrying_capactiy_PC2:Shoreline:EA033 +
   EA033 + 
   Shoreline +
   Settlement_date_grouping_finer"
@@ -29,14 +29,14 @@ fun_hedvig_brms_predicting(data = data, formula = formula, group = group)
 #MEDIUM
 
 data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_medium_group_scaled.tsv", show_col_types = F) %>%  
-  mutate("Combined_CCPC1_Size_EA033" = Carrying_capactiy_PC1*Shoreline*EA033) %>% 
-  mutate("Combined_CCPC2_Size_EA033" = Carrying_capactiy_PC2*Shoreline*EA033) %>% 
-  mutate("Combined_CCPC3_Size_EA033" = Carrying_capactiy_PC2*Shoreline*EA033) %>% 
   rename(group = Medium_only_merged_for_shared_language)
 
 formula <- "lg_count  ~ Carrying_capactiy_PC1 + 
   Carrying_capactiy_PC2 +
   Carrying_capactiy_PC3 +
+  Carrying_capactiy_PC1:Shoreline:EA033 +
+  Carrying_capactiy_PC2:Shoreline:EA033 +
+  Carrying_capactiy_PC3:Shoreline:EA033 +
   EA033 + 
   Shoreline +
   Settlement_date_grouping_finer"
