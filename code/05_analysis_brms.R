02e200f9a70c0ac61faeda9f8704bf74fea3d6d5
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-07-19 17:01:07 +0200

    Update 05_analysis_brms.R

--- code/05_analysis_brms.R
+++ code/05_analysis_brms.R
@@ -7,14 +7,23 @@ source("fun_def_brms_analysis_5.R")
 data <- read_tsv("output/processed_data/RO_Hedvig_aggregate_marck_group_scaled.tsv", show_col_types = F) %>%  
   rename(group = Marck_group)
 
+data %>% colnames()
+
 #inspired by
 #https://bookdown.org/ajkurz/Statistical_Rethinking_recoded/counting-and-classification.html#binomial-regression
 
 #model specs
-formula <- "lg_count  ~ Carrying_capactiy_PC1 + 
-  Carrying_capactiy_PC2 +
-#  Carrying_capactiy_PC1:Shoreline:EA033 +
-#  Carrying_capactiy_PC2:Shoreline:EA033 +
+formula <- "lg_count  ~   Annual_precipitation_mean+ 
+  Precipitation_seasonality_mean + 
+  Annual_temperature_mean + 
+  Temperature_seasonality_mean + 
+  Latitude_abs_mean +
+  NPP_terra_mean +
+  NPP_aqua_mean +
+#  Carrying_capactiy_PC1 + 
+#  Carrying_capactiy_PC2 +
+#  Carrying_capactiy_PC1:Shoreline +
+#  Carrying_capactiy_PC2:Shoreline +
   EA033 + 
   Shoreline +
   Settlement_date_grouping_finer"
