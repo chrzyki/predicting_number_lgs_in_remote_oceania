64857d1bc5ce2538367742daee96d6e373d57d5a
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-08-07 14:00:47 +0200

    Update 02_subregions_glottocodes_unnesting.R

--- code/02_subregions_glottocodes_unnesting.R
+++ code/02_subregions_glottocodes_unnesting.R
@@ -3,8 +3,9 @@ source("01_requirements.R")
 subregions <- read.delim("data/oceania_subregions.tsv", sep = "\t") %>% 
   dplyr::select(-Smallest_Island_group)
 
-read.delim("data/RO_polygons_grouped_with_languages.csv", sep = ",") %>% 
+subregions <- read.delim("data/RO_polygons_grouped_with_languages.csv", sep = ",") %>% 
   filter(!is.na(glottocodes)) %>%
+  filter(glottocodes != "") %>% 
   mutate(glottocodes = str_split(glottocodes, ",")) %>%
   unnest(glottocodes) %>% 
   mutate(Glottocode = trimws(glottocodes)) %>% 
@@ -12,7 +13,57 @@ read.delim("data/RO_polygons_grouped_with_languages.csv", sep = ",") %>%
   group_by(Glottocode) %>% 
   summarise(Smallest_Island_group = paste(Smallest_Island_group, collapse = ", ")) %>% 
   full_join(subregions, by = "Glottocode") %>% 
+  filter(!is.na(Glottocodes)) %>% 
+  filter(Glottocodes != "") 
+  
+subregions %>%
   write_tsv("data/oceania_subregions.tsv")
+  
+  
+##naming the new marck groups, SBZR  
+  
+polygons_df <- read.delim("data/RO_polygons_grouped_with_languages.csv", sep = ",") 
+
+polygons_df_distinct <- polygons_df %>% 
+  distinct(Marck_group, Smallest_Island_group)
+
+SBZR_df <- read_tsv("data/polygons_table_new_group.tsv", show_col_types = F) %>% 
+    dplyr::select(-Longitude, -Latitude) 
+  
+SBZR_df_names <- SBZR_df %>% 
+    full_join(polygons_df, by = join_by(Unique_ID)) %>% 
+    filter(!is.na(glottocodes)) %>% 
+    filter(glottocodes != "")  %>% 
+    distinct(Smallest_Island_group, cost_area_id) %>% 
+    group_by(cost_area_id) %>% 
+    mutate(cost_area_name = paste0(Smallest_Island_group, collapse = " + "),
+           n_cost = n()) %>% 
+    ungroup() %>% 
+    distinct(cost_area_name, cost_area_id, n_cost, .keep_all = T) %>%
+    mutate(dup = duplicated(cost_area_name, fromLast = T) + duplicated(cost_area_name, fromLast = F)) %>%  
+    mutate(cost_area_name = ifelse(dup >= 1, paste0(cost_area_name, "_", cost_area_id), cost_area_name))  %>% 
+  left_join(polygons_df_distinct, 
+            relationship = "many-to-many",
+            by = join_by(Smallest_Island_group)) %>% 
+  mutate(cost_area_name = ifelse(n_cost > 2, paste0(Marck_group), cost_area_name)) %>% 
+  distinct(cost_area_name, cost_area_id) %>% 
+  mutate(cost_area_name = ifelse(cost_area_name == "North Island (NZ) + South Island (NZ)", "Aotearoa", cost_area_name)) %>% 
+  mutate(cost_area_name = ifelse(cost_area_name == "North Marquesas + South Marquesas", "Marquesas", cost_area_name)) %>% 
+  full_join(SBZR_df, by = "cost_area_id") %>% 
+  rename(SBZR_group = cost_area_name)
+
+read.delim("data/RO_polygons_grouped_with_languages.csv", sep = ",") %>% View()
+#  full_join(SBZR_df_names) %>% 
+  write_csv("data/RO_polygons_grouped_with_languages.csv")
+
+#read_tsv("data/oceania_subregions.tsv") %>% 
+#  full_join(SBZR_df_names) %>% 
+#  write_tsv("data/oceania_subregions.tsv")
+
+
+
+
+
   
 #read_csv("data/Remote_Oceania_Political_complex_and_more/RO_polygons_grouped_with_languages.csv") %>%
 #  filter(!is.na(glottocodes)) %>%
