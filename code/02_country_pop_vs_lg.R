7995a5127f651b7b9662f1c464e38a8d82572845
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-08-09 17:42:23 +0200

    Update 02_country_pop_vs_lg.R

--- code/02_country_pop_vs_lg.R
+++ code/02_country_pop_vs_lg.R
@@ -70,3 +70,29 @@ combined %>%
   
 ggsave("output/plots/number_of_languags_vs_pop_1950_log10.png", width = 7, height = 7)
 ggsave("../latex/number_of_languags_vs_pop_1950_log10.png", width = 7, height = 7)
+
+
+###COUNTRY VS CARRYING CAPCITY
+COUNTRY_NAMES <- read_tsv("data/country_names.tsv", show_col_types = F)
+
+
+Island_group_summarised_COUNTRY <- read_tsv("output/processed_data/RO_Hedvig_aggregate_country_group_scaled.tsv", show_col_types = F) %>% 
+  inner_join(COUNTRY_NAMES, by ="COUNTRY NAME") %>% 
+  left_join(combined, by = join_by(ISO2_code)) %>% 
+  mutate(`Carrying_capactiy_PC1:Shoreline` = abs(1-Carrying_capactiy_PC1) * Shoreline) %>% 
+  mutate(`Carrying_capactiy_PC2:Shoreline` = Carrying_capactiy_PC2 * Shoreline)
+
+Island_group_summarised_COUNTRY %>% 
+dplyr::select(lg_count, Carrying_capactiy_PC1, Carrying_capactiy_PC2,`Population 1950` ,`Population 1950 log10`, Shoreline, `Carrying_capactiy_PC1:Shoreline`, `Carrying_capactiy_PC2:Shoreline`) %>% 
+  pairs.panels(method = "pearson", # correlation method
+               hist.col = "#a3afd1",# "#a9d1a3","",""),
+               density = TRUE,  # show density plots
+               ellipses = F, # show correlation ellipses
+               cex.labels= 0.7,
+               #           smoother= T,
+               cor=T,
+               lm=T,
+               ci = T, 
+               cex.cor = 1,stars = T)
+
+
