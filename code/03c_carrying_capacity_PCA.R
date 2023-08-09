1843695b374e55a939b4fa067aacda8bc5a7fda5
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-08-09 17:42:33 +0200

    Update 03c_carrying_capacity_PCA.R

--- code/03c_carrying_capacity_PCA.R
+++ code/03c_carrying_capacity_PCA.R
@@ -169,3 +169,42 @@ ggsave("../latex/PCA_contributions_medium_PC3.png", width = 12, height = 10)
 
 ###############################################################################
 ###############################################################################
+
+
+
+
+#COUNTRY
+data_fn_country <- "output/processed_data/RO_Hedvig_aggregate_country_group_scaled.tsv"
+
+data <- read_tsv(file = data_fn_country, show_col_types = F)  %>% 
+  column_to_rownames("COUNTRY NAME") %>% 
+  dplyr::select(Annual_precipitation_mean, 
+                Precipitation_seasonality_mean, 
+                Annual_temperature_mean, 
+                Temperature_seasonality_mean, 
+                Latitude_abs_mean,
+                NPP_terra_mean,
+                #                NPP_terra_var,
+                NPP_aqua_mean 
+                #               NPP_aqua_var
+  ) %>% 
+  mutate_all(as.numeric) 
+
+found_pcs <- find_pcs(data = data)
+
+writeLines(
+  found_pcs$nScree_summary_string,
+  con = file.path(OUTPUTDIR, "PCA_nScree_summary_carrying_country.txt")
+)
+
+carrying_capactiy_PCA_country_df <- found_pcs$data_pca_df[,1:found_pcs$maxcol]  %>% #because the first col is the group we need to do plus one
+  rename(Carrying_capactiy_PC1 = "PC1",
+         Carrying_capactiy_PC2 = "PC2",
+         `COUNTRY NAME` = ID)
+
+carrying_capactiy_PCA_country_df$Carrying_capactiy_PC1 <- modEvA::range01(carrying_capactiy_PCA_country_df$Carrying_capactiy_PC1) 
+carrying_capactiy_PCA_country_df$Carrying_capactiy_PC2 <- modEvA::range01(carrying_capactiy_PCA_country_df$Carrying_capactiy_PC2)
+
+read_tsv(data_fn_country, show_col_types = F) %>% 
+  full_join(carrying_capactiy_PCA_country_df, by = "COUNTRY NAME") %>% 
+  write_tsv(data_fn_country)
