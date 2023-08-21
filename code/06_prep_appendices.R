9373fbf61c493e241c773903af721ffdbb6b54d7
HedvigS <hedvig.skirgard@gmail.com>
2023-08-21 12:21:55 +0200

    Update 06_prep_appendices.R

--- code/06_prep_appendices.R
+++ code/06_prep_appendices.R
@@ -23,18 +23,21 @@ medium_effects <- read_tsv("output/results/brms_medium_full_effects_table.tsv", 
   dplyr::select(term, Estimate, `Est.Error`, `l-95% CI`, `u-95% CI`, `Does 95% interval straddle zero?`, Bulk_ESS, Tail_ESS)
 
 colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\_", " ")
+colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\%", "\\\\%")
+colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\:", "\\: ")
+
 
 #xtable printing
 
 cap <- "Table of BRMS model outcomes, shared-language island groups (all observations included)."
 lbl <- "BRMS_effects_medium"
-align <- c("r","p{3cm}","p{3cm}","p{3cm}", "p{3cm}", "p{3cm}", "p{3cm}", "p{3cm}", "p{3cm}") 
+align <- c("r","p{5cm}","p{2cm}","p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}") 
 
 fn <- "../latex/BRMS_effects_medium.tex"
 
 medium_effects %>% 
   xtable(caption = cap, label = lbl,
-         digits = 0, 
+         digits = 3, 
          align = align) %>% 
   xtable::print.xtable(file = fn, sanitize.colnames.function = function(x){x},
                        include.rownames = FALSE, math.style.negative = F,
