509d7b8c955f3423310ccebc5e5b82ba8e41590b
HedvigS <hedvig.skirgard@gmail.com>
2023-08-21 12:31:08 +0200

    Update 06_prep_appendices.R

--- code/06_prep_appendices.R
+++ code/06_prep_appendices.R
@@ -1,45 +1,14 @@
 source("01_requirements.R")
+source("fun_def_make_brms_appendix_xtable.R")
 
-medium_effects <- read_tsv("output/results/brms_medium_full_effects_table.tsv", show_col_types = F) %>% 
-  filter(variable == "Estimate"|
-          variable == "Est.Error"|
-           variable == "l-95% CI"|
-           variable == "u-95% CI"|
-           variable == "straddle_zero_95"|
-           variable == "Rhat"|
-           variable == "Bulk_ESS" |
-           variable == "Tail_ESS") %>% 
-  reshape2::dcast(term ~ variable, value.var = "value") %>% 
-  mutate(term = str_replace_all(term, "Settlement_date_grouping_finer", "Time depth")) %>% 
-  rename(`Does 95% interval straddle zero?`= straddle_zero_95) %>% 
-  mutate(Estimate = as.numeric(Estimate)) %>% 
-  mutate(Est.Error = as.numeric(Est.Error)) %>% 
-  mutate(`l-95% CI` = as.numeric(`l-95% CI`)) %>% 
-  mutate(`u-95% CI` = as.numeric(`u-95% CI`)) %>% 
-  mutate(Rhat = as.numeric(Rhat)) %>% 
-  mutate(Bulk_ESS = as.numeric(Bulk_ESS)) %>% 
-  mutate(Tail_ESS = as.numeric(Tail_ESS)) %>% 
-  mutate_if(.predicate = is.numeric, .funs = function(x){round(x, 3)}) %>% 
-  dplyr::select(term, Estimate, `Est.Error`, `l-95% CI`, `u-95% CI`, `Does 95% interval straddle zero?`, Bulk_ESS, Tail_ESS)
+make_brms_appendix_xtable(fn_in = "output/results/brms_medium_full_effects_table.tsv", 
+                          fn_out = "../latex/BRMS_effects_medium.tex",
+                          cap = "Table of BRMS model outcomes, shared-language island groups (all observations included).",
+                          lbl = "BRMS_effects_medium"
+ )
 
-colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\_", " ")
-colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\%", "\\\\%")
-colnames(medium_effects) <- str_replace_all(colnames(medium_effects), "\\:", "\\: ")
-
-
-#xtable printing
-
-cap <- "Table of BRMS model outcomes, shared-language island groups (all observations included)."
-lbl <- "BRMS_effects_medium"
-align <- c("r","p{5cm}","p{2cm}","p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}", "p{2cm}") 
-
-fn <- "../latex/BRMS_effects_medium.tex"
-
-medium_effects %>% 
-  xtable(caption = cap, label = lbl,
-         digits = 3, 
-         align = align) %>% 
-  xtable::print.xtable(file = fn, sanitize.colnames.function = function(x){x},
-                       include.rownames = FALSE, math.style.negative = F,
-                       booktabs = TRUE, floating = F) 
-
+make_brms_appendix_xtable(fn_in = "output/results/brms_SBZR_full_effects_table.tsv", 
+                          fn_out = "../latex/BRMS_effects_SBZR.tex",
+                          cap = "Table of BRMS model outcomes, overnight-distance island groups (all observations included).",
+                          lbl = "BRMS_effects_SBZR"
+)
