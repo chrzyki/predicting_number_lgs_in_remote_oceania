56acc27c3a44fd5f2d2bdfa38094a7a776cd7f67
HedvigS <hedvig.skirgard@gmail.com>
2023-08-23 23:56:44 +0200

    Update 06_prep_appendices.R

--- code/06_prep_appendices.R
+++ code/06_prep_appendices.R
@@ -16,30 +16,38 @@ make_brms_appendix_xtable(fn_in = "output/results/brms_SBZR_full_effects_table.t
 )
 
 #pol complex table
+# island groups
 subregions <- read_tsv( "output/processed_data/subregions.tsv", show_col_types = F) %>% 
   distinct(`Island group (overnight-sailing)` = SBZR_group, `Island group (shared language)` = Medium_only_merged_for_shared_language, Smallest_Island_group, glottocode = Glottocode_spec)
 
+# refs
+pol_complex_refs <- read_tsv("data/Remote_oceania_pol_complex.tsv", na = "", show_col_types = F)  %>% 
+  dplyr::select(`Political complexity (EA033)`, glottocode, citekey)
+
+# rename("$$\\textbf{\\parbox{2cm}{\\raggedright Method}}$$" = "Method") %>% 
+pol_complex_refs$citekey <- paste0("$$\\citet{",pol_complex_refs$citekey, "}$$")
+
+#table itself
 pol_complex <- readODS::read_ods("data/Remote_oceania_pol_complex_hedvig_code_latex.ods", sheet = 1) %>%
-  dplyr::select(glottocodes = glottocode, `Political complexity (EA033)`, Reference) %>% 
+  left_join(pol_complex_refs, 
+            relationship = "many-to-many",
+            by = join_by(`Political complexity (EA033)`, glottocode)) %>% 
+  dplyr::select(glottocodes = glottocode, `Political complexity (EA033)`, citekey) %>% 
+  distinct() %>% 
   mutate(glottocodes = ifelse(glottocodes == "fiji1243", "fiji1243,kada1285,sout2864,nort2843", glottocodes)) %>% 
   mutate(glottocodes = ifelse(glottocodes == "aust1304",  "aust1304,raiv1237,tubu1240,ruru1237", glottocodes)) %>% 
   mutate(glottocodes = ifelse(glottocodes == "maor1246", "maor1246,mori1267", glottocodes)) %>% 
   mutate(glottocodes = str_split(glottocodes, ",")) %>% 
   unnest(cols = c(glottocodes)) %>% 
   rename(glottocode = glottocodes) %>% 
-  left_join(subregions, by = "glottocode") %>% 
+  left_join(subregions, by = "glottocode", relationship = "many-to-many") %>% 
   filter(!is.na(`Island group (shared language)`)) %>% 
   group_by(`Island group (overnight-sailing)`, `Island group (shared language)`, `Political complexity (EA033)`) %>% 
-  summarise(glottocodes = paste0(glottocode, collapse = ", "), 
-            references = paste0(Reference, collapse = "%"), .groups = "drop") %>% 
-  mutate(references = str_split(references, "%")) %>% 
-  unnest(cols = c(references)) %>% 
-  distinct() %>% 
-  group_by(`Island group (overnight-sailing)`) %>% 
-  mutate(references = paste0(references, collapse = "; "), .groups = "drop") %>% 
-  distinct(`Island group (overnight-sailing)`, `Island group (shared language)`, `Political complexity (EA033)`, glottocodes, references)
+  summarise(glottocodes = paste0(glottocode, collapse = ", "),
+            References = paste0(citekey, collapse = ", "), .groups = "drop") %>% 
+  distinct(`Island group (overnight-sailing)`, `Island group (shared language)`, `Political complexity (EA033)`, glottocodes, References)
   
-
+#write xtable
 fn_out = "../latex/appendix_pol_complex_xtable.tex"
 cap <- "Table of political complexity values (EA033)."
 lbl <- "appendix_pol_complex_xtable"
@@ -54,6 +62,6 @@ xtable(caption = cap, label = lbl,
                        include.rownames = FALSE, math.style.negative = F,tabular.environment = "longtable",
                        booktabs = TRUE, floating = F) 
   
-read_tsv("output/processed_data/references.tsv") %>% 
-  distinct() %>% 
-  write_tsv("output/processed_data/references.tsv")
+
+
+
