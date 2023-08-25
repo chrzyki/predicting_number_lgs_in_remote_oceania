3f9bcabb5bf6d061cea191608eab30b60fd318a7
HedvigS <hedvig.skirgard@gmail.com>
2023-08-26 00:53:30 +0200

    Update 02_get_glottolog_language_table.R

--- code/02_get_glottolog_language_table.R
+++ code/02_get_glottolog_language_table.R
@@ -12,7 +12,7 @@ if(dir.exists(paths = "output/processed_data/glottolog_language_table_wide_df.ts
 if(!dir.exists(exdir)){  
 source("fun_def_get_zenodo.R")
 
-glottolog_fn <- c("https://zenodo.org/record/7398887/files/glottolog/glottolog-cldf-v4.7.zip")
+glottolog_fn <- c("https://zenodo.org/record/437430/files/clld/glottolog-v3.0.zip")
 
 get_zenodo_dir(url = glottolog_fn, exdir = exdir, remove_git_commit_dir = T)
 }
