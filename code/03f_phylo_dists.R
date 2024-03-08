4b0151097bd0aedcbd32d053aeee75827aa356bd
HedvigS <hedvig.skirgard@gmail.com>
2024-03-08 13:42:11 +0100

    Update 03f_phylo_dists.R

--- code/03f_phylo_dists.R
+++ code/03f_phylo_dists.R
@@ -1,46 +1,125 @@
 source("01_requirements.R")
 
-glottolog_df <- read_tsv("data/glottolog_language_table_wide_df.tsv", 
-                        show_col_types = F)
+glottolog_df <- read.delim("data/glottolog_language_table_wide_df.tsv", sep = "\t")
+
+  
+#  read_tsv("data/glottolog_language_table_wide_df.tsv", 
+#                        show_col_types = F)
+
+df_line <- glottolog_df %>% 
+  filter(Language_ID == "cent2080") %>% 
+  dplyr::select("subclassification") 
+
+df_line$subclassification  %>% str()
+tree_full <- ape::read.tree(text = df_line$subclassification)
+
+tree_full %>% str()
+
+
+df_line$subclassification %>% str()
+
+#get all labels (nodes and tips)
+oceanic_tip_labels <- oceanic_tree_full$tip.label
+oceanic_node_labels <- oceanic_tree_full$node.label
+oceanic_all_labels_df <- c(oceanic_tip_labels, oceanic_node_labels) %>% 
+  as.data.frame() %>%
+  rename(Language_ID = ".")
+
 
 polygons <- read_csv("data/RO_polygons_grouped_with_languages.csv", 
-                                          show_col_types = F) %>% 
+                     show_col_types = F) %>% 
   filter(!is.na(glottocodes)) %>%
   filter(glottocodes != "") %>% 
   mutate(glottocodes = str_split(glottocodes, ",")) %>%
   unnest(glottocodes) %>% 
   mutate(Glottocode = trimws(glottocodes)) 
-  
-#read in and prune tree
-gray_2009_mcct <- ape::read.nexus("data/trees/gray_et_al2009/original/a400-m1pcv-time.mcct.trees.gz")
 
-taxa <- read_csv(file = "data/trees/gray_et_al2009/taxa.csv", show_col_types = F) %>% 
-  rename(Glottocode = glottocode) #to conform to what glottolog does elsewhere 
 
-tree_removed_dups <- drop.tip(gray_2009_mcct, tip = gray_dup_to_remove)
 
-Gray_et_al_tree_tip.label_df <- tree_removed_dups$tip.label %>% 
+
+#subtree = TRUE
+
+
+#read in GB
+GB_df <- read_tsv(GB_binary_fn) %>% 
+  dplyr::select(Language_ID) %>% 
+  filter(Language_ID != "cent2060") %>% #removing proto-languages
+  filter(Language_ID != "east2449") %>% #removing proto-languages
+  filter(Language_ID != "poly1242") %>% #removing proto-languages
+  filter(Language_ID != "ocea1241") #removing proto-languages
+
+#make df of only things that are languages and oceanic
+oceanic_lgs <- read_tsv("output/processed_data/glottolog_oceanic_languages_df.tsv", show_col_types = F) %>% 
+  dplyr::select("Language_ID") %>% 
+  as.matrix() %>% 
+  as.vector()
+
+#find overlap between glottolog and GB
+overlap <- inner_join(GB_df, oceanic_all_labels_df, by = "Language_ID") %>% 
+  as.matrix() %>% 
+  as.vector()
+
+pruned_tree <-keep_as_tip(oceanic_tree_full, overlap)
+
+pruned_tree <- compute.brlen(pruned_tree, method = 1)
+
+pruned_tree  %>% ape::write.tree( "output/processed_data/trees/glottolog_tree_newick_GB_pruned.txt")
+
+polytomies_n <- pruned_tree$edge %>% 
   as.data.frame() %>% 
-  rename(taxon = ".") %>% 
-  left_join(taxa, by = "taxon") %>% 
-  left_join(glottolog_df, by = "Glottocode") 
+  group_by(V1) %>% 
+  summarise(n = n()) %>% 
+  filter(n > 2) %>% nrow()
 
-tree_removed_dups$tip.label <- Gray_et_al_tree_tip.label_df$Glottocode
+splits <- pruned_tree$edge[,1] %>% length()
 
-tips_to_drop <- tree_removed_dups$tip.label %>% 
-  as.data.frame() %>% 
-  rename(Glottocode = ".") %>% 
-  anti_join(polygons, by = "Glottocode") %>% 
-  distinct(Glottocode)
+message("The Glottolog Oceanic tree (pruned for Grambank matches) has ", splits, " splits. Out of these ",  round(polytomies_n/splits*100, 0), "% are non-binary.")
 
-gray_2009_mcct_pruned <- ape::drop.tip(phy = tree_removed_dups,tip = tips_to_drop$Glottocode, rooted = T)
+#prune tree to only languages in oceanic subgroup
+oceanic_tree <-keep_as_tip(oceanic_tree_full, oceanic_lgs)
 
-
-plot()
-is.rooted(gray_2009_mcct_pruned)
+oceanic_tree <- compute.brlen(oceanic_tree, method = 1)
 
 
-#gray_2009_mcct_pruned $edge.length = gray_2009_mcct_pruned$edge.length / 1000
+
+oceanic_tree  %>% ape::write.tree("output/processed_data/trees/glottolog_tree_newick_all_oceanic.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # https://github.com/grambank/grambank-analysed/blob/main/R_grambank/spatiophylogenetic_modelling/analysis/make_precisionmatrices.R
