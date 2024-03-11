d6c7674c2548a3ad83403bafb978fbc1119aeb40
HedvigS <hedvig.skirgard@gmail.com>
2024-03-11 16:15:12 +0100

    Update 03f_phylo_dists.R

--- code/03f_phylo_dists.R
+++ code/03f_phylo_dists.R
@@ -8,11 +8,6 @@ if(!file.exists(tree_fn)){
 
 tree_pruned <- ape::read.tree("output/processed_data/glottolog_tree_mala1545.tree")
 
-
-
-tree_dists <- adephylo::distTips(tree_full, )
-
-
 polygons <- read_csv("data/RO_polygons_grouped_with_languages.csv", 
                      show_col_types = F) %>% 
   filter(!is.na(glottocodes)) %>%
@@ -23,26 +18,8 @@ polygons <- read_csv("data/RO_polygons_grouped_with_languages.csv",
   distinct(Glottocode) %>% 
   left_join(glottolog_df, by = "Glottocode") 
 
-nodes_and_tips <- c(tree_full$tip.label, tree_full$node.label)
-
-tree_pruned <- keep_as_tip(tree_full, tips_and_nodes_to_keep = nodes_and_tips[nodes_and_tips %in% polygons$Language_level_ID])
-
-tree_pruned <- compute.brlen(tree_pruned, method = 1)
-
-tree_pruned$edge.length = tree_pruned$edge.length / 1000
-
-tree_pruned %>% ape::write.tree("output/processed_data/tree_glottolog_newick.tree")
+tree_dists <- adephylo::distTips(tree_pruned, method = "patristic", tips = polygons$Glottocode)
 
 
 
-
-
-
-
-
-
-phy_prec_mat %>% 
-  as.data.frame() %>% 
-  rownames_to_column("Rownames") %>% 
-write_tsv("output/processed_data/phy_prec_mat.tsv", na = "")
 
