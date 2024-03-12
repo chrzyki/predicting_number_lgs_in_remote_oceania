59aa45c3cc6ef0c15151bb317733972996108e5f
HedvigS <hedvig.skirgard@gmail.com>
2024-03-12 17:53:05 +0100

    Update 05_analysis_brms.R

--- code/05_analysis_brms.R
+++ code/05_analysis_brms.R
@@ -23,13 +23,17 @@ fun_brms_predicting(data = data, formula = formula, group = group)
 #MEDIUM
 
 data <- read_tsv("output/processed_data/RO_aggregate_medium_group_scaled.tsv", show_col_types = F) %>%  
-  rename(group = Medium_only_merged_for_shared_language)
+  rename(group = Medium_only_merged_for_shared_language) %>% 
+  mutate(spatial_id = group) %>% 
+  mutate(phylo_id = group)
 
 phylo_vcv <- readRDS("output/processed_data/tree_medium_vcv.rds")
 spatial_vcv <- readRDS("output/processed_data/spatial_vcv_medium.rds")
 data2 = list(phylo_vcv = phylo_vcv, spatial_vcv = spatial_vcv)
 
-formula <- lg_count  ~    environ_PC1*Shoreline +
+formula <- lg_count  ~    (1 | gr(phylo_id, cov = phylo_vcv)) +
+  (1 | gr(spatial_id, cov = spatial_vcv)) +
+  environ_PC1*Shoreline +
   environ_PC2*Shoreline +
   environ_PC3*Shoreline +
   mo(EA033) + 
@@ -37,8 +41,8 @@ formula <- lg_count  ~    environ_PC1*Shoreline +
 
 group = "medium"
 
-fun_brms_predicting(data = data, data2 = data2, formula = formula, group = group, control = "phylo", drop_one_out = FALSE )
+fun_brms_predicting(data = data, data2 = data2, formula = formula, group = group, control = "spatialphylo", drop_one_out = FALSE )
 
-fun_brms_predicting(data = data, data2 = data2, formula = formula, group = group, control = "none", drop_one_out = FALSE )
+fun_brms_predicting(data = data, data2 = data2, formula = formula, group = group, control = "phylo", drop_one_out = FALSE )
 
 
