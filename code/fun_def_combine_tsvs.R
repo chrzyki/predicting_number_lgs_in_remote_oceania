a0ce652a7b9d5dfa8f08fe8d18029c81a525b54c
HedvigS <hedvig.skirgard@gmail.com>
2024-03-13 18:11:59 +0100

    Create fun_def_combine_tsvs.R

---
+++ code/fun_def_combine_tsvs.R
@@ -0,0 +1,15 @@
+
+combine_tsvs <- function(fns = NULL){
+fns %>% 
+  map_df(
+    function(x) data.table::fread(x ,
+                                  encoding = 'UTF-8', header = TRUE, 
+                                  fill = TRUE, blank.lines.skip = TRUE,
+                                  sep = "\t", na.strings = "",
+    )   %>% 
+      mutate(filename = basename(x)
+             
+      ) 
+  ) 
+
+}
