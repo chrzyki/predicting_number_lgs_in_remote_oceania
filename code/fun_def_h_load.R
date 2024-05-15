8cef79fe299de42492b4d051320e2097924cd3e2
HedvigS <hedvig.skirgard@gmail.com>
2024-05-15 16:35:10 +0200

    rm groundhog from h_load

--- code/fun_def_h_load.R
+++ code/fun_def_h_load.R
@@ -4,36 +4,13 @@
 
 h_load <- function(pkg, 
                    verbose = FALSE, 
-                   groundhog_date = "2023-07-17",
                    ignore.deps = FALSE){
 
-
-#groundhogr set-up
-    if(!("groundhog" %in% rownames(installed.packages()))){
-      
-      install.packages("groundhog")
-      library(groundhog)
-  
-        }else{  
-      
-          library(groundhog)
-      
-          }
-  
-    groundhog_dir <- paste0("groundhog_libraries_", groundhog_date)
-  
-  if(!dir.exists(groundhog_dir)){
-    dir.create(groundhog_dir)
-  }
-  
-  groundhog::set.groundhog.folder(groundhog_dir)
-  
       for(p in pkg){
         
-      if(is.null(version) & (!(p %in% rownames(installed.packages())))){ #if no version is specified, check if it's installed and if not then go and install it as normal
-        
-        groundhog::groundhog.library(p, date = groundhog_date, 
-                                    ignore.deps = ignore.deps)
+      if((!(p %in% rownames(installed.packages())))){ 
+
+        install.packages(p)        
         
         if(verbose == T){
           cat(paste0("Installed ", p, ".\n"))}
