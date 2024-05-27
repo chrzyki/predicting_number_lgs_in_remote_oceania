437774cdd5747ce691d5f3676ccd77e39de9e1be
HedvigS <hedvig.skirgard@gmail.com>
2024-05-27 15:56:35 +0200

    Update testin_with_luke.R

--- code/testin_with_luke.R
+++ code/testin_with_luke.R
@@ -21,6 +21,13 @@ formula <- lg_count  ~    (1 | gr(phylo_id, cov = phylo_vcv)) +
   Shoreline*mo(Settlement_date_grouping_finer)
 
 
+iter = 30000
+warmup = 1000
+chains = 4
+cores = 4
+seed = 10
+ndraws = 10000
+
 output_poisson <-  brms::brm(data = data, 
                              data2 = data2,
                              family = poisson,
@@ -34,3 +41,24 @@ output_poisson <-  brms::brm(data = data,
                              seed = seed,
                              control = list(adapt_delta = 0.9),
                              backend="cmdstanr") 
+
+
+ms_full <- summary(output_poisson)
+
+
+
+### model output
+chain_1 <- output_poisson$fit@sim$samples[[1]] %>% as.data.frame()  %>% mutate(chain = "1")
+chain_2 <- output_poisson$fit@sim$samples[[2]] %>% as.data.frame()  %>% mutate(chain = "2")
+chain_3 <- output_poisson$fit@sim$samples[[3]] %>% as.data.frame()  %>% mutate(chain = "3")
+chain_4 <- output_poisson$fit@sim$samples[[4]] %>% as.data.frame()  %>% mutate(chain = "4")
+
+chain_joined <- suppressMessages(full_join(chain_1, chain_2)) %>% 
+  suppressMessages(full_join(chain_3)) %>%
+  suppressMessages(full_join(chain_4))
+
+df <- chain_joined %>% 
+  reshape2::melt(id.vars = "chain") %>%   
+  filter(str_detect(variable, "sd_phylo"))
+
+df$value %>% hist()
