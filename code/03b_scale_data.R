126dd4080038291482ddb084fe814c35eac34097
HedvigS <hedvig.skirgard@gmail.com>
2024-07-03 11:33:56 +0200

    Update 03b_scale_data.R

--- code/03b_scale_data.R
+++ code/03b_scale_data.R
@@ -28,7 +28,8 @@ data <- read_tsv("output/processed_data/RO_aggregate_SBZR_group.tsv", show_col_t
                 Settlement_date_grouping_finer, #9
                 ratio_coastline_to_area,#10
                 NPP_terra_mean,#11
-                NPP_aqua_mean #12
+                NPP_aqua_mean ,#12 
+                Melanesia_or_not #13
       ) 
 
 #log10 size variables to take out the oversized effect of large island groups, like south island aotearoa etc
@@ -83,7 +84,8 @@ data <- read_tsv("output/processed_data/RO_aggregate_medium_island.tsv", show_co
                 Settlement_date_grouping_finer, #9
                 ratio_coastline_to_area,#10
                 NPP_terra_mean,#12
-                NPP_aqua_mean#12
+                NPP_aqua_mean,#12
+                Melanesia_or_not #13
 )
 
 #log10 size variables to take out the oversized effect of large island groups, like south island aotearoa etc
@@ -136,7 +138,8 @@ data <- read_tsv("output/processed_data/RO_aggregate_COUNTRY_group.tsv", show_co
                 Settlement_date_grouping_finer, #9
                 ratio_coastline_to_area,#10
                 NPP_terra_mean,#12
-                NPP_aqua_mean#12
+                NPP_aqua_mean, #12
+                Melanesia_or_not #13
   )
 
 #log10 size variables to take out the oversized effect of large island groups, like south island aotearoa etc
