78df2a8e645672839422bdf55a4d50898ec988c0
HedvigS <hedvig.skirgard@gmail.com>
2024-10-08 14:03:54 +0200

    Update 01_check_used_functions.R

--- code/01_check_used_functions.R
+++ code/01_check_used_functions.R
@@ -1,13 +1,5 @@
 source("01_requirements.R")
-source("01_requirements_brms.R")
-
-if(!("SH.misc"%in% rownames(installed.packages()))){
-  remotes::install_github("HedvigS/SH.misc", ref = "3ad2758d63792e1e9216119b4b5bad269a3bf944")}
-library(SH.misc)
 
 fns <- list.files(path = ".", all.files = T, full.names = T, recursive = T, pattern = ".R$")
 
-SH.misc::credit_packages(fns = fns, output_dir = "../latex/", print_bibTeX = T, print_tex_citation_string = T, print_LaTeX_table = T, print_tsv = T)
-
-
-
+SH.misc::credit_packages(fns = fns, output_dir = "../latex/", extra_pkgs = NULL)
